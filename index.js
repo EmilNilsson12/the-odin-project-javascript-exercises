7fd2259bc722be06f10d7fa050f78f6cde15cc1e
Emil Nilsson <emil.git.nilsson@gmail.com>
2021-03-15 14:26:27 +0100

    Finish repeatString exercise, add style.css

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import { caesar } from './caesar/caesar.js';
 const caesarInputText = document.getElementById('input-caesar-text');
 const caesarInputNum = document.getElementById('input-caesar-num');
 const caesarBtn = document.getElementById('btn-caesar');
-const outputCaesar = document.getElementById('output-caesar');
+const ceasarOutput = document.getElementById('output-caesar');
 
 
 caesarBtn.addEventListener('click', () => {
-        outputCaesar.innerHTML = `Ciphered message: <br />"${caesar(caesarInputText.value, caesarInputNum.value)}"`;
+        ceasarOutput.innerHTML = `Ciphered message: <br />"${caesar(caesarInputText.value, caesarInputNum.value)}"`;
 })
 
 
@@ -30,3 +30,32 @@ function caesarClick(evt) {
 import { helloWorld } from './helloWorld/helloWorld.js'
 const helloWorldP = document.getElementById('output-hello-world');
 helloWorldP.innerHTML = helloWorld();
+
+
+
+
+import { repeatString } from './repeatString/repeatString.js';
+
+
+const repeatStringInputText = document.getElementById('input-repeat-string-text');
+const repeatStringInputNum = document.getElementById('input-repeat-string-num');
+const repeatStringBtn = document.getElementById('btn-repeat-string');
+const repeatStringOutput = document.getElementById('output-repeat-string');
+
+
+repeatStringBtn.addEventListener('click', () => {
+        repeatStringOutput.innerHTML =  `Message repeated ${repeatStringInputNum.value} times: <br />"${repeatString(repeatStringInputText.value, repeatStringInputNum.value)}"`;
+})
+
+
+//------------ Allows user to complete input with Enter key ------------
+caesarInputText.addEventListener('keyup', repeatStringClick);
+caesarInputNum.addEventListener('keyup', repeatStringClick);
+
+function repeatStringClick(evt) {
+        if (evt.key == 'Enter') {
+            evt.preventDefault();
+            repeatStringBtn.click();
+        }
+}
+//------------ Allows user to complete input with Enter key ------------
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+export const repeatString = function(str, repeats) {
+    if (repeats >= 0) {
 
+        let returnStr = '';
+        for (let i = 0; i < repeats; i++) {
+            returnStr += str;
+        }
+        return returnStr
+    }
+    else {
+        return "ERROR"
+    }
 }
 
-module.exports = repeatString
+//module.exports = repeatString
