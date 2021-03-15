2007d5373e92ea01677dc0d922223b18267b2d02
Emil Nilsson <emil.git.nilsson@gmail.com>
2021-03-15 13:40:19 +0100

    Finish caesar cipher exercise

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,44 @@
-const caesar = function() {
+// Input: (string, int)
+const caesar = function(msg, shift) {
+    
+    // Convert shift to int
+    shift = parseInt(shift);
 
+    // Convert msg to array
+    msg = msg.split('')
+
+    // Store shiftet msg in new array
+    let shiftedMsg = [];
+
+    // Replace each letter with shifted unicode value
+    msg.forEach(letter => {
+
+        let unicode = letter.charCodeAt();
+        let newUniCode = 0;
+        
+        // Check whether uppercase or lowercase
+        if (unicode >= 65 && unicode <= 90) {
+            newUniCode = ((unicode - 65 + shift) % 26) + 65;
+        }
+        else if (unicode >= 97 && unicode <= 122) {
+            newUniCode = ((unicode - 97 + shift) % 26) + 97;
+        }
+        else {
+            newUniCode = unicode
+        }
+
+        // Push shifted letter to shiftedMsg
+        const shiftedLetter = String.fromCharCode(newUniCode);
+        console.log("shiftedLetter: ", shiftedLetter);
+        shiftedMsg.push(shiftedLetter);
+    });
+
+    // Covert array back to string
+    shiftedMsg = shiftedMsg.join('');
+
+    return shiftedMsg
 }
 
-module.exports = caesar
+export { caesar };
+//module.exports = caesar;
+
---
+++ index.js
@@ -0,0 +1,35 @@
+import { caesar } from './caesar/caesar.js';
+
+
+const caesarInputText = document.getElementById('input-caesar-text');
+const caesarInputNum = document.getElementById('input-caesar-num');
+const caesarBtn = document.getElementById('btn-caesar');
+const outputCaesar = document.getElementById('output-caesar');
+
+
+caesarBtn.addEventListener('click', () => {
+        outputCaesar.innerHTML = `Ciphered message: <br />"${caesar(caesarInputText.value, caesarInputNum.value)}"`;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+//------------ Allows user to complete input with Enter key ------------
+caesarInputText.addEventListener('keyup', caesarClick);
+caesarInputNum.addEventListener('keyup', caesarClick);
+
+function caesarClick(evt) {
+        if (evt.key == 'Enter') {
+            evt.preventDefault();
+            caesarBtn.click();
+        }
+}
+//------------ Allows user to complete input with Enter key ------------
