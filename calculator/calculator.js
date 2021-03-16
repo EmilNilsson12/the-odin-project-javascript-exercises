ad696fb623ef65c9a047b593dfec575a105702ed
Emil Nilsson <emil.git.nilsson@gmail.com>
2021-03-16 15:22:20 +0100

    Finish calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,54 @@
-function add () {
-	
+function add (num1, num2) {
+	return num1 + num2
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return num1 - num2
 }
 
-function sum () {
-	
+function sum (arr) {
+	if (arr.length > 0) {
+		return arr.reduce((accumulator, value) => {
+			return accumulator + value
+		})
+	}
+	else {
+		return 0
+	}
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((accumulator, value) => {
+		return accumulator * value
+	})
 }
 
-function power() {
-	
+function power(base, exponent) {
+	return Math.pow(base, exponent)
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num == 0) {
+		return 1
+	}
+	else {
+		return num * factorial(num - 1)
+	}
 }
 
-module.exports = {
+export { 
+	add
+	,subtract
+	,sum
+	,multiply
+	,power
+	,factorial };
+
+/* module.exports = {
 	add,
 	subtract,
 	sum,
 	multiply,
     power,
 	factorial
-}
+} */
--- index.js
+++ index.js
@@ -18,8 +18,8 @@ caesarInputNum.addEventListener('keyup', caesarClick);
 
 function caesarClick(evt) {
         if (evt.key == 'Enter') {
-            evt.preventDefault();
-            caesarBtn.click();
+                evt.preventDefault();
+                caesarBtn.click();
         }
 }
 //------------ Allows user to complete input with Enter key ------------
@@ -44,7 +44,7 @@ const repeatStringOutput = document.getElementById('output-repeat-string');
 
 
 repeatStringBtn.addEventListener('click', () => {
-        repeatStringOutput.innerHTML =  `Message repeated ${repeatStringInputNum.value} times: <br />"${repeatString(repeatStringInputText.value, repeatStringInputNum.value)}"`;
+        repeatStringOutput.innerHTML = `Message repeated ${repeatStringInputNum.value} times: <br />"${repeatString(repeatStringInputText.value, repeatStringInputNum.value)}"`;
 })
 
 
@@ -54,8 +54,8 @@ repeatStringInputNum.addEventListener('keyup', repeatStringClick);
 
 function repeatStringClick(evt) {
         if (evt.key == 'Enter') {
-            evt.preventDefault();
-            repeatStringBtn.click();
+                evt.preventDefault();
+                repeatStringBtn.click();
         }
 }
 //------------ Allows user to complete input with Enter key ------------
@@ -70,7 +70,7 @@ const reverseStringOutput = document.getElementById('output-reverse-string');
 
 
 reverseStringBtn.addEventListener('click', () => {
-        reverseStringOutput.innerHTML =  `Message reversed: <br />"${reverseString(reverseStringInputText.value)}"`;
+        reverseStringOutput.innerHTML = `Message reversed: <br />"${reverseString(reverseStringInputText.value)}"`;
 })
 
 
@@ -80,8 +80,8 @@ caesarInputNum.addEventListener('keyup', reverseStringClick);
 
 function reverseStringClick(evt) {
         if (evt.key == 'Enter') {
-            evt.preventDefault();
-            reverseStringBtn.click();
+                evt.preventDefault();
+                reverseStringBtn.click();
         }
 }
 //------------ Allows user to complete input with Enter key ------------
@@ -100,7 +100,7 @@ const sumAllOutput = document.getElementById('output-sum-all');
 
 
 sumAllBtn.addEventListener('click', () => {
-        sumAllOutput.innerHTML =  `Sum of all numbers between "${sumAllInputNum1.value}" and "${sumAllInputNum2.value}": <br />${sumAll(parseInt(sumAllInputNum1.value), parseInt(sumAllInputNum2.value))}`;
+        sumAllOutput.innerHTML = `Sum of all numbers between "${sumAllInputNum1.value}" and "${sumAllInputNum2.value}": <br />${sumAll(parseInt(sumAllInputNum1.value), parseInt(sumAllInputNum2.value))}`;
 });
 
 
@@ -118,10 +118,10 @@ leapYearBtn.addEventListener('click', () => {
         const isLeapYear = leapYears(parseInt(leapYearInput.value));
 
         if (isLeapYear) {
-                leapYearOutput.innerHTML =  `The year "${leapYearInput.value}" is a leap year!`;
+                leapYearOutput.innerHTML = `The year "${leapYearInput.value}" is a leap year!`;
         }
         else {
-                leapYearOutput.innerHTML =  `The year "${leapYearInput.value}" is not a leap year!`;
+                leapYearOutput.innerHTML = `The year "${leapYearInput.value}" is not a leap year!`;
         }
 });
 
@@ -145,4 +145,94 @@ ctofBtn.addEventListener('click', () => {
 ftocBtn.addEventListener('click', () => {
         const newTemp = ctof(parseInt(ftocInput.value));
         tempConversionOutput.innerHTML = `${ftocInput.value} degrees farenheit is equal to ${newTemp} degrees celcius.`
+});
+
+
+
+import {
+        add
+        , subtract
+        , sum
+        , multiply
+        , power
+        , factorial
+} from './calculator/calculator.js';
+
+
+
+const factorialBtn = document.getElementById('btn-factorial');
+
+
+const calculatorOutput = document.getElementById('output-calculator');
+
+
+const addBtn = document.getElementById('btn-add');
+addBtn.addEventListener('click', () => {
+        const addInput = document.querySelectorAll('.input-add');
+        calculatorOutput.innerHTML = `${addInput[0].value} + ${addInput[1].value} = ${add(parseInt(addInput[0].value), parseInt(addInput[1].value))}.`
+});
+
+
+const subtractBtn = document.getElementById('btn-subtract');
+subtractBtn.addEventListener('click', () => {
+        const subtractInput = document.querySelectorAll('.input-subtract');
+        calculatorOutput.innerHTML = `${subtractInput[0].value} - ${subtractInput[1].value} = ${subtract(parseInt(subtractInput[0].value), parseInt(subtractInput[1].value))}.`
+});
+
+
+const sumBtn = document.getElementById('btn-sum');
+sumBtn.addEventListener('click', () => {
+        const sumInput = document.querySelectorAll('.input-sum');
+        calculatorOutput.innerHTML = `Sum of nums: <br /> ${sumInput[0].value}`;
+
+        const numsToSum = [
+                parseInt(sumInput[0].value)
+        ];
+        for (let i = 1; i < sumInput.length; i++) {
+                calculatorOutput.innerHTML += ` + ${sumInput[i].value} `;
+                numsToSum.push(parseInt(sumInput[i].value));
+        }
+        calculatorOutput.innerHTML += ` = ${sum(numsToSum)} `;
+});
+const moreNumsSum = document.getElementById('more-nums-sum');
+moreNumsSum.addEventListener('click', () => {
+        moreNumsSum.insertAdjacentHTML('beforebegin', '<input type="number" class="input-sum">')
+});
+
+
+const multiplyBtn = document.getElementById('btn-multiply');
+multiplyBtn.addEventListener('click', () => {
+        const multiplyInput = document.querySelectorAll('.input-product');
+        calculatorOutput.innerHTML = `Sum of nums: <br /> ${multiplyInput[0].value}`;
+
+        const numsToMultiply = [
+                parseInt(multiplyInput[0].value)
+        ];
+        for (let i = 1; i < multiplyInput.length; i++) {
+                if(multiplyInput[i].value == '') continue;
+                calculatorOutput.innerHTML += ` * ${multiplyInput[i].value} `;
+                numsToMultiply.push(parseInt(multiplyInput[i].value));
+        }
+        calculatorOutput.innerHTML += ` = ${multiply(numsToMultiply)} `;
+
+});
+const moreNumsProduct = document.getElementById('more-nums-product');
+moreNumsProduct.addEventListener('click', () => {
+        moreNumsProduct.insertAdjacentHTML('beforebegin', '<input type="number" class="input-product">')
+});
+
+const powerBtn = document.getElementById('btn-power');
+powerBtn.addEventListener('click', () => {
+        const base = document.getElementById('input-power-base');
+        const exponent = document.getElementById('input-power-exponent');
+        
+        const exponentiation = power(parseInt(base.value), parseInt(exponent.value));
+        calculatorOutput.innerHTML = `${base.value} <sup>${exponent.value}</sup> is equal to ${exponentiation}`
+});
+
+factorialBtn.addEventListener('click', () => {
+        const factorialInput = document.getElementById('input-factorial');
+        
+        const factorialResult = factorial(parseInt(factorialInput.value));
+        calculatorOutput.innerHTML = `${factorialInput.value} factorial is equal to ${factorialResult}`
 });
