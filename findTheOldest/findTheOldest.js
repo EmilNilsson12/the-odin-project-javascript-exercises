523053fc88395e7c38a2cff3dd5044ac66dc69da
Emil Nilsson <emil.git.nilsson@gmail.com>
2021-03-16 17:27:49 +0100

    Finish findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
 
+    const now = new Date();
+    const currentYear = now.getFullYear();
+    
+    let oldestAge = 0;
+    let oldestPerson;
+    let age;
+
+    for (person of people) {
+
+        if (person.yearOfDeath == undefined) {
+            age = currentYear - person.yearOfBirth;
+        }
+        else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson
 }
 
 module.exports = findTheOldest
